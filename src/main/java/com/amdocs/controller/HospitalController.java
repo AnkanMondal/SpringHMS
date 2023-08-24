a85f548bd73a93e5cf1e8ccc1144f6c1bc8bcbcb
Ankan Mondal <ankanmondal45@gmail.com>
2023-08-24 11:13:57 +0530

    Update HospitalController.java

--- src/main/java/com/amdocs/controller/HospitalController.java
+++ src/main/java/com/amdocs/controller/HospitalController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HospitalController {
     @Autowired
     private HospitalRepository hospitalRepository;
+    private patientRepository patientRepository;
 
     @GetMapping("/hospitals")
     public String listHospitals(Model model) {
@@ -18,5 +19,24 @@ public class HospitalController {
         return "hospital/list";
     }
     
-    // Add other CRUD operations here
+    @GetMapping("/addPatient")
+    public String addPatient(@ModelAttribute Patient patient) {
+        patientRepository.save(patient);
+        return "redirect:/";
+    }
+
+    @GetMapping("/updatePatient/{id}")
+    public String updatePatient(@ModelAttribute Patient patient) {
+        Hospital hospital = hospitalRepository.findById(id).orElse(null);
+        model.addAttribute("hospital", hospital);
+        model.addAttribute("patients", patientRepository.findAll());
+        patientRepository.save(patient);
+        return "updatePatient";
+    }
+
+    @GetMapping("/deletePatient/{id}")
+    public String deletePatient(@PathVariable Long id) {
+        patientRepository.deleteById(id);
+        return "redirect:/";
+    }
 }
